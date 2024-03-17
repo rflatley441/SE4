facfe738e8c395b99e1986253b92aaa24097e318
rseal2 <rseal2@huskers.unl.edu>
2024-03-16 21:24:25 -0500

    player pfp

--- backend/player.py
+++ backend/player.py
@@ -1,11 +1,11 @@
 from tile import Tile
 
-
 class Player:
-    def __init__(self, userID, score=0):
+    def __init__(self, userID, score=0, profile_picture=None):
         self.userID = userID
         self.score = score
         self.hand = []
+        self.profile_picture = profile_picture
 
     def updateScore(self, pointsToAdd):
         self.score += pointsToAdd
@@ -13,3 +13,9 @@ class Player:
 
     def addTileToHand(self, tile):
         self.hand.append(tile)
+
+    def setProfilePicture(self, profile_picture):
+        self.profile_picture = profile_picture
+
+    def getProfilePicture(self):
+        return self.profile_picture
