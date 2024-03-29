3f72c9943b2c8cfbc9fda4f285eaa38482d54708
rseal2 <rseal2@huskers.unl.edu>
2024-03-29 15:59:06 -0500

    websocket initial setup

---
+++ frontend/src/socket.js
@@ -0,0 +1,35 @@
+import { reactive } from "vue";
+import { io } from "socket.io-client";
+
+
+const production = "wss://intro-se-sockets-lab-server.fly.dev";
+
+const development = "ws://localhost:8080"
+
+export const state = reactive({
+  connected: false,
+  fooEvents: [],
+  barEvents: []
+});
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+
+export const socket = io(URL);
+
+socket.on("connect", () => {
+  state.connected = true;
+  
+});
+
+socket.on("disconnect", () => {
+  state.connected = false;
+});
+
+socket.on("foo", (...args) => {
+  state.fooEvents.push(args);
+});
+
+socket.on("bar", (...args) => {
+  state.barEvents.push(args);
+});
