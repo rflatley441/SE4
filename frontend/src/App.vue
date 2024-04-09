a4f3f1ef9eab70753b6d8c5f059db6f6d27591d5
christinatinley <ctinley04@gmail.com>
2024-04-09 15:03:49 -0500

    Cleans up App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,11 @@
 <template>
-  <!-- <router-view/> -->
-  {{ message }}
+  <router-view/>
 </template>
 
 <script>
-import { state, socket } from './socket'; // replace './socket' with the actual path to the file where `state` and `socket` are defined
-
-// import axios from 'axios';
-// import GameBoard from './components/GameBoard.vue';
 
 export default {
   name: 'App',
-  data() {
-    return {
-      message: "Not connected"
-    }
-  },
-  components: {
-    // GameBoard,
-  },
-  async mounted() {
-    //const response = await axios.post('http://127.0.0.1:5000/api', { message: "hi" });
-    //alert(response.data.message)
-      socket.on('connect', () => {
-      this.message = state.connected ? "Connected" : "Not Connected";
-    });
-  }
 }
 </script>
 
