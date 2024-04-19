17929d1dc5553660382fb824835fd0164bd9e233
christinatinley <ctinley04@gmail.com>
2024-04-19 13:31:25 -0500

    Fixes an issue with the socket setup

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -8,4 +8,6 @@ export const state = reactive({
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://127.0.0.1:5000";
 
-export const socket = io(URL);
+const socket = io(URL);
+
+export default socket;
