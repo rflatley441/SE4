cd21d3a6f53c6b45d81170cd77ed6b84eeae1e23
rseal2 <rseal2@huskers.unl.edu>
2024-04-21 16:48:05 -0500

    added volume sliding bar on settings

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,51 +1,43 @@
 <template>
     <div id="app">
         <NavBar />
-      <div id="content">
-        <div id="signuptitle">User Settings</div>
-        <div id="inputsContainer">
-            <div class="innerBox">
-          <div class="inputLabel">Username</div>
-          <div class="inputHolder">
-            <input
-              type="text"
-              class="inputBox"
-              v-model="username"
-              placeholder="Username"
-            />
-          </div>
-          <div class="passwordContainer">
-    <div class="inputLabel">Password</div>
-    <div class="forgotPassword">Forgot password?</div>
-</div>
-          <div class="inputHolder" style="padding-bottom: 20px">
-            <input
-              type="password"
-              class="inputBox"
-              v-model="password"
-              placeholder="Password"
-            />
-          </div>
-  
-          <div class="inputLabel" style="padding-top: 10px">Email</div>
-          <div class="inputHolder" style="padding-bottom: 10px">
-            <input
-              type="text"
-              class="inputBox"
-              v-model="email"
-              placeholder="Email Address"
-            />
-          </div>
-        </div>
+        <div id="content">
+            <div id="title">User Settings</div>
+            <div id="inputsContainer">
+                <div class="innerBox">
+                    <div class="inputLabel">Username</div>
+                    <div class="inputHolder">
+                        <input type="text" class="inputBox" v-model="username" placeholder="Username" />
+                    </div>
+                    <div class="passwordContainer">
+                        <div class="inputLabel">Password</div>
+                        <div class="forgotPassword">Forgot password?</div>
+                    </div>
+                    <div class="inputHolder" style="padding-bottom: 20px">
+                        <input type="password" class="inputBox" v-model="password" placeholder="Password" />
+                    </div>
+
+                    <div class="inputLabel" style="padding-top: 10px">Email</div>
+                    <div class="inputHolder" style="padding-bottom: 10px">
+                        <input type="text" class="inputBox" v-model="email" placeholder="Email Address" />
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="volumeContainer">
+            <div id="volumeTitle">Volume</div>
+            <div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">   
+            </div>
+            </div>
 
         </div>
-
-      </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .passwordContainer {
+</template>
+
+<style scoped>
+.passwordContainer {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -57,22 +49,28 @@
     cursor: pointer;
     font-size: 15px;
 }
-  #content {
-    margin-top: 20px; /* Adjust this value as needed */
+
+#content {
+    margin-top: 20px;
+    /* Adjust this value as needed */
 }
-  .innerBox {
-    width: 80%; /* Adjust this value to make the box narrower */
-    margin-right: 20px; /* Adjust this value to move the box to the right */
+
+.innerBox {
+    width: 80%;
+    /* Adjust this value to make the box narrower */
+    margin-right: 20px;
+    /* Adjust this value to move the box to the right */
 }
-  #app {
+
+#app {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100vw;
-  }
+}
 
- 
-  #signuptitle {
+
+#title {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -82,69 +80,125 @@
     font-weight: 80;
     color: black;
     font-family: Arial, Helvetica, sans-serif;
-  }
-  
-  #inputsContainer {
+}
+
+#inputsContainer {
     background: #b3daff;
     width: 500px;
     padding-left: 60px;
     padding-top: 40px;
     padding-bottom: 40px;
-    margin-left:550px;
-  }
-  
-  .inputHolder {
+    margin-left: 550px;
+}
+
+.inputHolder {
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-  .inputBox {
+}
+
+.inputBox {
     width: 900px;
     border: none;
     font-size: 30px;
     height: 60px;
     margin-top: 5px;
     padding-left: 10px;
-  }
-  
-  .inputLabel {
+}
+
+.inputLabel {
     font-size: 35px;
     font-weight: 600;
     color: black;
     font-family: Arial, Helvetica, sans-serif;
     text-align: left;
-  }
-  #passwordrequirments {
+}
+
+#volumeTitle {
+    position: absolute; /* Change this from relative to absolute */
+    top: -40px; /* Adjust this value to move the title up */
+    left: 0; /* Adjust this value to move the title to the left */
+    font-size: 35px; /* Adjust this value to change the title size */
+    font-weight:bold;
+}
+#volumeContainer { 
+    background: #b3daff;
+    width: 990px;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    padding-left: 60px;
+    padding-right: 60px;
+    margin-top: 50px;
+    position: relative;
+    display: flex; /* Add this line */
+    justify-content: center; /* Add this line */
+    align-items: center; /* Add this line */
+    flex-direction: column; /* Add this line */
+}
+
+.slidecontainer {
+  width: 80%;
+}
+
+/* The slider itself */
+.slider {
+  -webkit-appearance: none;  /* Override default CSS styles */
+  appearance: none;
+  width: 100%; /* Full-width */
+  height: 25px; /* Specified height */
+  background: #000000; /* Grey background */
+  outline: none; /* Remove outline */
+  opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+  -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+  transition: opacity .2s;
+}
+
+/* Mouse-over effects */
+.slider:hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+
+/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  width: 25px; /* Set a specific slider handle width */
+  height: 25px; /* Slider handle height */
+  background: #04AA6D; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
+#passwordrequirments {
     font-size: 15px;
     font-weight: 100;
     color: black;
     font-family: Arial, Helvetica, sans-serif;
     text-align: right;
-  }
-  
-  #footer {
+}
+
+#footer {
     display: flex;
     justify-content: space-evenly;
     padding-top: 20px;
-  }
-  
-  .footText {
+}
+
+.footText {
     font-size: 35px;
     font-weight: 300;
     color: #2490f3;
     cursor: pointer;
-  }
-  </style>
-  
-  <script>
+}
+</style>
+
+<script>
+
 import NavBar from '@/components/NavBar.vue';
 export default {
     name: "SettingsView",
     components: {
         NavBar
     },
- }
- 
+}
 
-  </script>
-  
+
+</script>
